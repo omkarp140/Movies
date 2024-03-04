8a98a653bf7a33dd33460e22b50aab943168fa00
Omkar PatilKarade <omkar.patilkarade@simplifai.ai>
2024-03-04 17:52:32 +0530

    feat: added angular routing

---
+++ AngularMovies.Web/src/app/components/actors/create-actors/create-actors.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-actors',
+  templateUrl: './create-actors.component.html',
+  styleUrls: ['./create-actors.component.css']
+})
+export class CreateActorsComponent {
+
+}
